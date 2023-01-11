efbeed4c8673b8c38f94cc41323b7dd75d36f182
uhaseeb <uhaseeb731@gmail.com>
2023-01-11 18:05:50 +0500

    Update class and objects of employee and vehicle class

--- class-and-objects.py
+++ class-and-objects.py
@@ -1,23 +1,26 @@
 class Employee:
-    company = 'Company 1'
+    company = 'Cogent Labs'
 
-    def __init__(self, name, email, designation):
+    def __init__(self, name, email, designation, company):
         self.name = name
         self.email = email
         self.designation = designation
+        self.company = company
 
     def display(self):
         print("Name of employee is", self.name)
+        print("Email of employee is", self.email)
         print("Designation of employee is", self.designation)
+        print("My company is:", self.company)
 
     @classmethod
     def company_intro(cls):
         print("Welcome to the company..", cls.company)
 
 
-emp1 = Employee('Employee 1', 'employee1@gmail.com', 'Software Engineer')
+emp1 = Employee('Employee 1', 'employee1@gmail.com', 'Software Engineer', 'Paramsync')
 emp1.display()
-emp2 = Employee('Employee 2', 'employee2@gmail.com', 'Web Designer')
+emp2 = Employee('Employee 2', 'employee2@gmail.com', 'Web Designer', 'Flync')
 emp2.display()
 Employee.company_intro()
 
